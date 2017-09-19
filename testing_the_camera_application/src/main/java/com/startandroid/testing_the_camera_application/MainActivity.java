c3402fcc7bf86c9cc3373bd4735dade9a0c66e63
pavel081215 <pavelzag12@gmail.com>
2017-09-19 20:30:10 +0300

    19/09/17 17/04 Testing the Camera Application

---
+++ testing_the_camera_application/src/main/java/com/startandroid/testing_the_camera_application/MainActivity.java
@@ -0,0 +1,44 @@
+package com.startandroid.testing_the_camera_application;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private File output = null;
+    private static final int CONTENT_REQUEST = 1745;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+
+        output = new File(dir, "CameraContentDemo.jpeg");
+        i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(output));
+        startActivityForResult(i, CONTENT_REQUEST);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CONTENT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setDataAndType(Uri.fromFile(output), "image/jpeg");
+                startActivity(i);
+                finish();
+            }
+        }
+    }
+}
